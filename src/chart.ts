2d23884f08fc7cd0dbe97395d3e9d19c7a4aab17
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-11-15 21:28:41 -0500

    Finished the chart code

--- src/chart.ts
+++ src/chart.ts
@@ -1,5 +1,7 @@
 import { createMyChart } from "./modules/chart_module";
-
+const myLoanPaymentHolder = document.querySelector('#monthlyPayment');
+const myTotalPayment = document.querySelector('#totalPayment');
+const myTotalInterest = document.querySelector('#totalInterest');
 const ctx = document.getElementById("myChart") as HTMLCanvasElement;
 const myMortgageForm = document.getElementById(
   "mortgageForm"
@@ -11,8 +13,14 @@ interface MortgageData {
   startYear: number;
   interestRate: number;
 }
-
-
+const setLoanData = function(monthly:number, total:number, interest:number) {
+  myLoanPaymentHolder.textContent = "$" + monthly.toFixed(2);
+  myTotalPayment.textContent = "$" + total.toFixed(2);
+  myTotalInterest.textContent = "$" + interest.toFixed(2);
+}
+const everyNthElement = function<T>(arr:T[], n:number):T[]{
+  return arr.filter((_,i)=>i%n===0);
+}
 myMortgageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target as HTMLFormElement);
@@ -21,26 +29,27 @@ myMortgageForm.addEventListener("submit", (e) => {
   const MONTHLY_INTEREST_RATE = Number(data.interestRate) / 100 / 12;
   const PRINCIPAL = Number(data.loanAmount) - Number(data.downPayment);
   let remainingPrincipal = PRINCIPAL;
+  let totalInterest = 0;
   const MONTHLY_PAYMENT = PRINCIPAL * (MONTHLY_INTEREST_RATE * (1 + MONTHLY_INTEREST_RATE) ** TOTAL_NUMBER_OF_MONTHS) / ((1 + MONTHLY_INTEREST_RATE) ** TOTAL_NUMBER_OF_MONTHS - 1);
   const mortgageData = Array.from({ length: TOTAL_NUMBER_OF_MONTHS }, (_, i) => {
     const year = Number(data.startYear) + Math.floor(i / 12);
     const month = i % 12;
     
     const interest = remainingPrincipal * MONTHLY_INTEREST_RATE;
-    const principal = MONTHLY_PAYMENT - interest;
-  
-     remainingPrincipal = remainingPrincipal - principal;
+    const monthlyPrincipal = MONTHLY_PAYMENT - interest;
+    totalInterest += interest;
+     remainingPrincipal = remainingPrincipal - monthlyPrincipal;
     return {
       year,
       month,
       interest,
-      principal,
+      monthlyPrincipal,
       remainingPrincipal,
     };
   }
 );
-console.table(mortgageData)
-  
-  createMyChart(ctx, mortgageData);
+  setLoanData(MONTHLY_PAYMENT,Number(data.loanAmount), totalInterest);
+  const dataToSendToChart = [...everyNthElement(mortgageData, 12).filter(data=>data.year!=mortgageData[mortgageData.length-1].year), mortgageData[mortgageData.length-1]];
+  createMyChart(ctx, dataToSendToChart , Number(data.loanTerm));
 });
 
--- src/modules/chart_module.ts
+++ src/modules/chart_module.ts
@@ -4,11 +4,11 @@ type ChartData = {
   year: number;
   month: number;
   interest: number;
-  principal: number;
+  monthlyPrincipal: number;
   remainingPrincipal: number;
 };
 
-export const createMyChart = (ctx: HTMLCanvasElement, dta: ChartData[]) =>{
+export const createMyChart = (ctx: HTMLCanvasElement, dta: ChartData[], loanTerm:number ) =>{
   if(Chart.getChart(ctx)){
     Chart.getChart(ctx).destroy();
   }
@@ -20,21 +20,77 @@ return new Chart(ctx, {
 
     datasets: [
       {
-        label: "Remaining Principal",
+        label: "Remaining Balance",
         data: dta.map((d) => d.remainingPrincipal),
-        borderColor: "blue",
+        borderColor: "#0d6efd",
+        backgroundColor:"#0d6efd",
         type: "line",
         order: 0,
+      },
+      {
+        label: "Principal",
+        data: dta.map((d) => d.monthlyPrincipal),
+        borderColor: "#074297",
+        backgroundColor: "#074297",
+        type: "bar",
+        yAxisID: "payments",
+      },
+      {
+        label: "Interest",
+        data: dta.map((d) => d.interest),
+        backgroundColor: "#03214b",
+        borderColor: "#03214b",
+        type: "bar",
+        yAxisID: "payments",
       },
     ],
   },
   options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        stacked: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+        ticks: {
+          stepSize: loanTerm,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        stacked: false,
+        title: {
+          display: true,
+          text: "Balance",
+        },
+      },
+      payments: {
+        min: 0,
+        stacked: true,
+        beginAtZero: true,
+        position: "right",
+        grid: {
+          drawOnChartArea: false,
+        },
+        title: {
+          display: true,
+          text: "Monthly Payments",
+        },
+      },
+    },
     plugins: {
       title: {
         display: true,
         text: "Mortgage Over Years",
       },
+      
     },
+    interaction:{
+      mode: 'index',
+    }
   },
 });
 }
